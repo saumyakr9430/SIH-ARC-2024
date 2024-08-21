cb9d00ca235ac335d9372a0b4eca9c5e5b5da482
Saumya kumar <133251033+saumyakr9430@users.noreply.github.com>
2024-08-21 05:39:39 +0530

    Add files via upload

--- ALGO/DES3.py
+++ ALGO/DES3.py
@@ -1,20 +1,19 @@
-
-from Crypto.Cipher import AES
+from Crypto.Cipher import DES3
 import datetime
 from Crypto.Random import get_random_bytes
 
-# 1. create a text
+# 1. create text
 plaintext = b'I am sending the encrypted message.'
 
 # get the start datetime
 st = datetime.datetime.now()
 
-# 2. create the key
-key = get_random_bytes(16)
+# 2. create the key, avoid Option 3
+key = DES3.adjust_key_parity(get_random_bytes(24))
+cipher = DES3.new(key, DES3.MODE_CFB)
 
-# 3. encrypt the message by key and sign it
-cipher = AES.new(key, AES.MODE_EAX)
-ciphertext, tag = cipher.encrypt_and_digest(plaintext)
+# 3. encrypt the message by key
+ciphertext = cipher.iv + cipher.encrypt(plaintext)
 
 # get the end datetime
 et = datetime.datetime.now()
@@ -22,7 +21,6 @@ et = datetime.datetime.now()
 elapsed_time = et - st
 print('Execution time:', elapsed_time, 'seconds')
 
-print(plaintext) # print original message
-print(cipher.nonce)
+print(plaintext)
 print(key)
 print(ciphertext)
--- ALGO/bowlfish.py
+++ ALGO/bowlfish.py
@@ -1,4 +1,5 @@
-from Crypto.Cipher import DES
+from Crypto.Cipher import Blowfish
+from struct import pack
 import datetime
 
 # 1. create text
@@ -8,17 +9,22 @@ plaintext = b'I am sending the encrypted message.'
 st = datetime.datetime.now()
 
 # 2. create the key
-key = b'-8B key-'
-cipher = DES.new(key, DES.MODE_OFB)
+key = b'An arbitrarily long key'
 
-# 3. encrypt the message by key
-ciphertext = cipher.iv + cipher.encrypt(plaintext)
+# 3. encrypt the message
+bs = Blowfish.block_size
+cipher = Blowfish.new(key, Blowfish.MODE_CBC)
+plen = bs - len(plaintext) % bs
+padding = [plen]*plen
+padding = pack('b'*plen, *padding)
+ciphertext = cipher.iv + cipher.encrypt(plaintext + padding)
 
 # get the end datetime
 et = datetime.datetime.now()
 # get execution time
 elapsed_time = et - st
 print('Execution time:', elapsed_time, 'seconds')
+
 
 print(plaintext)
 print(key)
